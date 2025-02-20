c4da90e1444a764c6648c9c8fe1b46ebfab84d33
sujay-182002 <sujayvishu31@gmail.com>
2025-02-20 17:10:34 +0530

    Add files via upload

---
+++ src/main/java/com/example/e learning/E learningappApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E learning {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E learning.class, args);
+	}
+
+}
